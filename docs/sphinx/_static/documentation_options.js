c5cf3d7d455277571042cbba2992ac6753aa54ad
Fabrice Le Fessant <fabrice.le_fessant@origin-labs.com>
2020-08-26 19:24:40 +0200

    xxx

--- docs/sphinx/_static/documentation_options.js
+++ docs/sphinx/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'master (b650ce6) (2020/08/26 19:16)',
+    VERSION: 'master (57ee581) (2020/08/26 19:24)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
