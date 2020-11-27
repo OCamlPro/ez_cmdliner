27ed90e7006bc34cb89c3de92d3fa207c92def5d
lefessan <fabrice@lefessant.net>
2020-11-27 21:47:46 +0000

    Deploying to gh-pages from  @ 3f333948e3eb06e97e00e94e18d0af372f21e38a 🚀

---
+++ sphinx/_static/documentation_options.js
@@ -0,0 +1,10 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'z-2020-09-22-multiple-unit-and-drom (87f4f77) (2020/09/22 23:28)',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+};
